7e8958b104896d40a1d5fda07fbb735f07f0ef3c
TJBaumgartner <tnorth6@gmail.com>
2023-05-02 15:52:23 -0700

    prints menu skeleton

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import './style.css';
-import  createContent from './loadContent';
+//import './style.css';
+//import  createContent from './loadContent';
 
-const divContent = createContent();
-document.body.appendChild(divContent)
+//const divContent = createContent();
+//document.body.appendChild(divContent)
+import createContent from './loadContent';
+const content = createContent();
+const btn = document.createElement('button');
+btn.addEventListener('click', buttonCheck);
+function buttonCheck(){
+    console.log('button works');
+}
+const contentContainer = document.getElementById('content');
+contentContainer.appendChild(content)
+contentContainer.appendChild(btn);
