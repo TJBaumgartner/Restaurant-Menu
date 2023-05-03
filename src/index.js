c30eb523f7ef2c4d281cc5447052cf591c001e64
TJBaumgartner <tnorth6@gmail.com>
2023-05-02 17:09:47 -0700

    header created and buttons

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-//import './style.css';
-//import  createContent from './loadContent';
-
-//const divContent = createContent();
-//document.body.appendChild(divContent)
-import createContent from './loadContent';
-const content = createContent();
-const btn = document.createElement('button');
-btn.addEventListener('click', buttonCheck);
-function buttonCheck(){
-    console.log('button works');
-}
+import './style.css';
+import {header} from './loadContent';
+const displayHeader = header();
 const contentContainer = document.getElementById('content');
-contentContainer.appendChild(content)
-contentContainer.appendChild(btn);
+contentContainer.appendChild(displayHeader)
--- src/loadContent.js
+++ src/loadContent.js
@@ -1,8 +1,31 @@
-export default function createContent() {   
 
-    const header = document.createElement('h1');
-    header.textContent = "This is my Restaurant";  
-    const image = document.createElement('img');
+    const header = () => {
+        const headContainer = document.createElement('div')
+        headContainer.classList.add('headContainer')
+
+        const head = document.createElement('h1');
+        head.innerHTML = 'Pizza Place'
+        headContainer.appendChild(head);
+        
+        let navElements = ['Home', 'Menu', 'Contact'];
+        const listContainer = document.createElement('div');
+        listContainer.classList.add('navList');
+        let listElement = document.createElement('ul');
+        let listItem = document.createElement('button');
+        headContainer.appendChild(listContainer)
+        listContainer.appendChild(listElement);
+        let numberofElements = navElements.length;
+
+        for(let i = 0; i < numberofElements; i++){
+            listItem.textContent = navElements[i];
+            listElement.appendChild(listItem);
+            listItem = document.createElement('button');
+        }
+        
+        return headContainer;
+    }
+
+    /**const image = document.createElement('img');
     image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/800px-Good_Food_Display_-_NCI_Visuals_Online.jpg";
     const bodyContent = document.createElement('p');
     bodyContent.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
@@ -11,5 +34,8 @@ export default function createContent() {
     contentContainer.appendChild(header);
     contentContainer.appendChild(image);
     contentContainer.appendChild(bodyContent);
-    return contentContainer
-};
+    return contentContainer*/
+
+export{
+    header,
+}
