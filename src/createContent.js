f566b7e11da217b8102a4c0bb305445468306e7b
TJBaumgartner <tnorth6@gmail.com>
2023-05-03 10:42:23 -0700

    renamed loadContent to createContent

--- src/createContent.js
+++ src/createContent.js
@@ -1,5 +1,5 @@
 
-    const header = () => {
+const header = () => {
         const headContainer = document.createElement('div')
         headContainer.classList.add('headContainer')
 
@@ -7,6 +7,7 @@
         head.innerHTML = 'Pizza Place'
         headContainer.appendChild(head);
         
+        //create nav buttons
         let navElements = ['Home', 'Menu', 'Contact'];
         const listContainer = document.createElement('div');
         listContainer.classList.add('navList');
@@ -15,16 +16,40 @@
         headContainer.appendChild(listContainer)
         listContainer.appendChild(listElement);
         let numberofElements = navElements.length;
-
         for(let i = 0; i < numberofElements; i++){
             listItem.textContent = navElements[i];
-            listElement.appendChild(listItem);
-            listItem = document.createElement('button');
-        }
-        
-        return headContainer;
-    }
+            if(i == 0) {
+                listItem.setAttribute('id', 'homeButton');
+                listElement.appendChild(listItem);
+                listItem = document.createElement('button');
+            }
+            if(i == 1) {
+                listItem.setAttribute('id', 'menuButton');
+                listElement.appendChild(listItem);
+                listItem = document.createElement('button');
+            }
+            if(i == 2) {
+                listItem.setAttribute('id', 'contactButton');
+                listElement.appendChild(listItem);
+                listItem = document.createElement('button');
+            }
 
+        }  
+        return headContainer;
+}
+const bodyContent = () =>{
+    const bodyContainer = document.createElement('div')
+    bodyContainer.classList.add('bodyContainer')
+
+    const bodyHead = document.createElement('h1');
+    bodyHead.innerHTML = 'Welcome to the Pizza place where we make the best pizza you will ever taste!'
+    bodyContainer.appendChild(bodyHead);
+
+    const bodyText = document.createElement('p');
+    bodyText.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    bodyContainer.appendChild(bodyText);
+
+}
     /**const image = document.createElement('img');
     image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/800px-Good_Food_Display_-_NCI_Visuals_Online.jpg";
     const bodyContent = document.createElement('p');
@@ -38,4 +63,6 @@
 
 export{
     header,
+    bodyContent,
+
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './style.css';
-import {header} from './loadContent';
+import {header} from './createContent';
+import { bodyContent } from './createContent';
 const displayHeader = header();
 const contentContainer = document.getElementById('content');
 contentContainer.appendChild(displayHeader)
+
+//COOOKIE PIZZA FOR BRE
+//ANCHOVIE PIZZA FOR BRE
