fc3c314e0071155640917589b931ce8abc4d4903
TJBaumgartner <tnorth6@gmail.com>
2023-05-03 21:51:48 -0700

    more menu styling

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import clearContent from './clearContent';
 import {menuContent} from './menuPage'
 import {homeContent} from './homePage';
 import {contactContent} from './contactPage';
+import {footer} from './createContent';
+
 loadPage();
 const contentContainer = document.getElementById('content');
+const displayFooter = footer();
 
 const menuButton = document.getElementById('menuButton');
 const homeButton = document.getElementById('homeButton');
@@ -13,14 +16,17 @@ const contactButton = document.getElementById('contactButton');
 homeButton.addEventListener('click', function(){
     clearContent();
     contentContainer.appendChild(homeContent());
+    contentContainer.appendChild(displayFooter);
 });
 menuButton.addEventListener('click', function(){
     clearContent();
     contentContainer.appendChild(menuContent());
+    contentContainer.appendChild(displayFooter);
 });
 contactButton.addEventListener('click', function(){
     clearContent();
     contentContainer.appendChild(contactContent());
+    contentContainer.appendChild(displayFooter);
 });
 //COOOKIE PIZZA FOR BRE
 //ANCHOVIE PIZZA FOR BRE
